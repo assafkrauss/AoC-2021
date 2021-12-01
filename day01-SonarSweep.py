bae5640f9166083f6ed4450662d636a40fcb5c0f
Assaf Krauss <assaf.krauss@gmail.com>
2021-12-02 00:07:32 +0200

    Day 1 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day01-SonarSweep.py
+++ day01-SonarSweep.py
@@ -1,10 +1,24 @@
 def main():
     f = open("day01.txt")
     lines = f.readlines()
+
+    # part I
     inc_count = 0
     for i in range(1, len(lines)):
         if int(lines[i - 1]) < int(lines[i]):
             inc_count += 1
+
+    print(inc_count)
+
+    # part II
+    depths = list(map(lambda x: int(x), lines))
+    window = sum(depths[0:2])
+    inc_count = 0
+    for i in range(3, len(depths)):
+        new_window = window - depths[i - 3] + depths[i]
+        if new_window > window:
+            inc_count += 1
+        window = new_window
 
     print(inc_count)
 
