bae2e9d5edf92c142eef628650f24cdc5dfaa0f2
Assaf Krauss <assaf.krauss@gmail.com>
2021-12-02 09:34:34 +0200

    Day 2 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day02-Dive.py
+++ day02-Dive.py
@@ -2,6 +2,7 @@ def main():
     f = open("day02.txt")
     lines = f.readlines()
 
+    # part I:
     horizontal, depth = 0, 0
     for line in lines:
         [direction, val] = line.strip().split()[:2]
@@ -11,6 +12,20 @@ def main():
             depth += int(val)
         else:  # assuming direction == "up"
             depth -= int(val)
+
+    print(horizontal * depth)
+
+    # part II:
+    horizontal, depth, aim = 0, 0, 0
+    for line in lines:
+        [direction, val] = line.strip().split()[:2]
+        if direction == "forward":
+            horizontal += int(val)
+            depth += aim * int(val)
+        elif direction == "down":
+            aim += int(val)
+        else:  # assuming direction == "up"
+            aim -= int(val)
 
     print(horizontal * depth)
 
