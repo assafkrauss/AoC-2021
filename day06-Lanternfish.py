fb6de1d1ca54f4fa6bca89b377c5868b256f6d46
Assaf Krauss <assaf.krauss@gmail.com>
2021-12-06 10:20:38 +0200

    Day 6 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day06-Lanternfish.py
+++ day06-Lanternfish.py
@@ -2,18 +2,21 @@ def main():
     f = open("day06.txt")
     line = f.readline()
     all_fish = list(map(lambda x: int(x), line.split(',')))
-    cycle = 9 * [0]
-    for fish in all_fish:
-        cycle[fish] += 1
 
-    for i in range(80):
-        new_fish = cycle[0]
-        for j in range(len(cycle) - 1):
-            cycle[j] = cycle[j + 1]
-        cycle[8] = new_fish
-        cycle[6] += cycle[8]
+    parts = [80, 256]
+    for part in parts:
+        cycle = 9 * [0]
+        for fish in all_fish:
+            cycle[fish] += 1
 
-    print(sum(cycle))
+        for i in range(part):
+            new_fish = cycle[0]
+            for j in range(len(cycle) - 1):
+                cycle[j] = cycle[j + 1]
+            cycle[8] = new_fish
+            cycle[6] += cycle[8]
+
+        print(sum(cycle))
 
 
 if __name__ == '__main__':
