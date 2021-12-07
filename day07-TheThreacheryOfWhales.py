cadf7a2cc6e871f236c5bd00015965635303703c
Assaf Krauss <assaf.krauss@gmail.com>
2021-12-07 09:17:12 +0200

    Day 7 - refactor
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day07-TheThreacheryOfWhales.py
+++ day07-TheThreacheryOfWhales.py
@@ -1,18 +1,23 @@
+def calc_fuel(points, fuel_func):
+    min_fuel = -1
+    for i in range(min(points), max(points) + 1):
+        fuel = 0
+        for p in points:
+            fuel += fuel_func(abs(p - i))
+        if min_fuel == -1:
+            min_fuel = fuel
+        else:
+            min_fuel = min(min_fuel, fuel)
+    return min_fuel
+
+
 def main():
     f = open("day07.txt")
     line = f.readline()
     all_crabs = list(map(lambda x: int(x), line.split(',')))
 
-    min_fuel = -1
-    for i in range(min(all_crabs), max(all_crabs) + 1):
-        fuel = 0
-        for crab in all_crabs:
-            fuel += abs(crab - i)
-        if min_fuel == -1:
-            min_fuel = fuel
-        else:
-            min_fuel = min(min_fuel, fuel)
-    print(min_fuel)
+    # part I
+    print(calc_fuel(all_crabs, lambda x: x))
 
 
 if __name__ == '__main__':
