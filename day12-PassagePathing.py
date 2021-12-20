1d40314b567eb759ca8bf1b5275f28717da640cb
Assaf Krauss <assaf.krauss@gmail.com>
2021-12-20 21:51:07 +0200

    Day 12 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day12-PassagePathing.py
+++ day12-PassagePathing.py
@@ -32,6 +32,36 @@ class CaveGraph:
         self._all_paths_util('start', 'end', visited, path, all_paths)
         return all_paths
 
+    def _all_paths_with_doubles_util(self, src, dest, visited, path, all_paths, double_cave):
+        if str.islower(src):
+            visited[src] += 1
+        path.append(src)
+
+        if src == dest:
+            all_paths.append(list(path))
+        else:
+            for v in self._connections[src]:
+                if v not in visited.keys() or visited[v] == 0 or visited[v] == 1 and v == double_cave:
+                    self._all_paths_with_doubles_util(v, dest, visited, path, all_paths, double_cave)
+
+        path.pop()
+        if src in visited:
+            visited[src] -= 1
+
+    def all_paths_with_doubles(self):
+        low_caves = [c for c in self._connections.keys() if str.islower(c) and c not in ['start', 'end']]
+        total_of_all_paths = []
+        for low_cave in low_caves:
+            visited = {}
+            for v in self._connections.keys():
+                if str.islower(v):
+                    visited[v] = 0
+            path = []
+            all_paths = []
+            self._all_paths_with_doubles_util('start', 'end', visited, path, all_paths, low_cave)
+            total_of_all_paths += all_paths
+        return total_of_all_paths
+
 
 def main():
     f = open("day12.txt")
@@ -40,6 +70,13 @@ def main():
     cg = CaveGraph(lines)
     paths = cg.all_paths()
     print(len(paths))
+
+    paths = cg.all_paths_with_doubles()
+    # no idea why there are duplications, so let's filter those out:
+    no_dups = set()
+    for p in paths:
+        no_dups.add(tuple(p))
+    print(len(no_dups))
 
 
 if __name__ == '__main__':
