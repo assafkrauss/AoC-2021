1eb371b5e21ef2d4f87744df67ecf83010759dca
Assaf Krauss <assaf.krauss@gmail.com>
2021-12-21 21:48:34 +0200

    Day 13 - 2nd star
    Signed-off-by: Assaf Krauss <assaf.krauss@gmail.com>

--- day13-TransparentOrigami.py
+++ day13-TransparentOrigami.py
@@ -35,13 +35,23 @@ def main():
         folds.append(fold)
         i += 1
 
-    for i in range(1):
+    for i in range((len(folds))):
         if folds[i][0] == 'y':
             dots = fold_up(dots, folds[i][1])
         else:
             dots = fold_left(dots, folds[i][1])
+        if i == 0:
+            print(len(dots))
 
-    print(len(dots))
+    min_x = min(dots, key=lambda dot: dot[0])[0]
+    max_x = max(dots, key=lambda dot: dot[0])[0]
+    min_y = min(dots, key=lambda dot: dot[1])[1]
+    max_y = max(dots, key=lambda dot: dot[1])[1]
+    for y in range(min_y, max_y + 1):
+        row = ''
+        for x in range(min_x, max_x + 1):
+            row += '#' if (x, y) in dots else ' '
+        print(row)
 
 
 if __name__ == '__main__':
